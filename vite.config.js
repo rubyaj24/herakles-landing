91e856ea5d9f05852b03584633ace4fa0d633235
rubyaj24 <amaljithmvinod@gmail.com>
2025-06-01 18:54:55 +0530

    Add deployment scripts and configure Vite build settings

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/herakles-landing/', // Replace with your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
